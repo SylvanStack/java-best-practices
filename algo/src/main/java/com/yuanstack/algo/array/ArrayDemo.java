fe4b9d20ac4ccd82ca69d2bf53fe423c22c4a3f6
Hansiyuan131 <422295068@qq.com>
2024-02-26 16:02:11 +0800

    feat: init

---
+++ algo/src/main/java/com/yuanstack/algo/array/ArrayDemo.java
@@ -0,0 +1,13 @@
+package com.yuanstack.algo.array;
+
+/**
+ * 数组测试
+ *
+ * @author: siyuan.han
+ * @date: 2024-02-26 15:37
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+        System.out.println("################################");
+    }
+}
