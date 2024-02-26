bf708a60d59c2a16682029bcb48718ec5c3f45df
Hansiyuan131 <422295068@qq.com>
2024-02-26 15:34:44 +0800

    feat: init

---
+++ runtime-load/src/main/java/com/yuanstack/runtime/load/RuntimeLoadApplication.java
@@ -0,0 +1,13 @@
+package com.yuanstack.runtime.load;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntimeLoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuntimeLoadApplication.class, args);
+    }
+
+}
---
+++ runtime-load/src/test/java/com/yuanstack/runtime/load/RuntimeLoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanstack.runtime.load;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntimeLoadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
